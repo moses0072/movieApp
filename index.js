effa0922e590e04d8360f987837fe593c643c143
Moussa Ghaddar <78150453+moses0072@users.noreply.github.com>
2021-04-13 15:27:52 +0200

    Added the module body-parser. users are able to get a list of movies , to add a movie and to delete a movie

--- index.js
+++ index.js
@@ -1,69 +1,181 @@
 //Import express and create the server
-const express = require('express');
-const morgan = require('morgan');
+const express = require('express'),
+	  morgan = require('morgan'),
+	  bodyParser = require('body-parser');
+	  
 const app = express();
 
+app.use(bodyParser.json());
+
 let topMovies = [
-	{
-		title: 'Harry Potter'
+	{		
+		title: 'Harry Potter',
+		genre: {
+			name:''
+		},
+		director: {
+			name:''
+		}		
 	},
 	{
-		title: 'Lord of the Rings'
+		title: 'The Matrix',
+		genre: { 
+			name: 'Science fiction'
+		},
+		director: {
+			name: 'Lana and Lilly Wachowski, the sisters born on 1965 and 1967, are american film and television directors, writers and producers.'
+		}
 	},
 	{
-		title: 'Mission Imposible'
+		title: 'Mission Imposible',
+		genre: {
+			name:'',
+		},
+		director: {
+			name:'',
+		},
+
 	},
 	{
-		title: 'Heat'
+		title: 'Heat',
+		genre: 'crime',
+		director: {
+			name: 'Michael Mann was born on 1943 in Chicago. He is an American director, screenwriter and producer of film and television who is best known for his distinctive brand of stylized crime drama' 
+		},
 	},
 	{
-		title: 'Bad boys'
+		title: 'Bad boys',
+		genre: {
+			name:''
+		},
+		director: {
+			name:''
+		}
 	},
 	{
-		title: 'Avengers End Game'
+		title: 'Avengers',
+		genre: {
+			name:''
+		},
+		director: {
+			name:''
+		}
 	},
 	{
-		title: 'Iron Man'
+		title: 'Iron Man',
+		genre: {
+			name:''
+		},
+		director: {
+			name:''
+		}
 	},
 	{
-		title: 'Spiderman'
+		title: 'Spiderman',
+		genre: {
+			name:''
+		},
+		director: {
+			name:''
+		}
 	},
 	{
-		title: 'Superman'
+		title: 'Man of steel',
+		genre: {
+			name:''
+		},
+		director: {
+			name:''
+		}
 	},
 	{
-		title: 'Batman'
+		title: 'Batman',
+		genre: {
+			name:''
+		},
+		director: {
+			name:''
+		}
 	},
 	{
-		title: 'Lonly Ranger'
+		title: 'Lonly Ranger',
+		genre: {
+			name:''
+		},
+		director: {
+			name:''
+		}
 	},
 	{
-		title: 'Night at the museum'
+		title: 'Night at the museum',
+		genre: {
+			name:''
+		},
+		director: {
+			name:''
+		}
 	},
 	{
-		title: 'Forest Gump'
+		title: 'Forest Gump',
+		genre: {
+			name:''
+		},
+		director: {
+			name:''
+		}
 	}
 
 ];
-
-
-
-
-//get the top 10 movies
-app.get('/movies', (req, res) => {
-	res.json(topMovies.slice(0, 10));
-});
-
-//get the starting request
-app.get('/', (req, res) => {
-	res.send('Welcome to myFlex movies!');
-});
 
 //Get the documentation
 app.use(express.static('public'));
 app.get('/documentation', (req, res) => {
 	        res.sendFile('public/documentation.html', {root: __dirname});
 });
+
+
+//get all the movies 
+app.get('/movies', (req, res) => {
+	res.json(topMovies);
+});
+
+
+//get a movie title
+app.get('movies/:title', (req, res) => {
+	res.json(topMovies.find((movie) =>
+		{return movie.title === req.params.title}));
+});
+
+//Add data for a new film
+app.post('/movies', (req, res) => {
+	let newMovie = req.body;
+	if (!newMovie.title) {
+		const message = 'Missing movie title in the request body';
+		res.status(400).send(message);
+	} else {
+		movies.push(newMovie);
+		res.status(201).send(newMovie);
+	}
+});
+
+//Deletes a movie from the List
+app.delete('/movies/:title', (req, res) => {
+	let movie = topMovies.find((movie) => {return movie.title === req.params.title});
+	if (movie) {
+		movies = topMovies.filter((obj) => {return obj.title !== req.params.title});
+		res.status(201).send('Movie ' + req.params.title + ' was deleted.');
+	}
+});
+
+
+
+
+//get the starting request
+app.get('/', (req, res) => {
+	res.send('Welcome to myFlex movies!');
+});
+
+
 
 //logs into the Terminal
 app.use(morgan('common'));
