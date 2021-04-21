b0a429920e0909112a0501dec98b90a7f545d5ab
Moussa Ghaddar <78150453+moses0072@users.noreply.github.com>
2021-04-21 11:21:38 +0200

    removed the authentication from /user to allow new users to register

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.get('/movies/director/:Name', passport.authenticate('jwt', { session: false}
 
 
 // Allow new users to register
-app.post('/users', passport.authenticate('jwt', { session: false}), (req, res) => {
+app.post('/users', (req, res) => {
 	Users.findOne({ Username: req.body.Username })
 		.then((user) => {
 			if (user) {
