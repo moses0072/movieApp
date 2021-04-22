59655e06257bad0445a9c361ef8cf5e5f4abc8e1
Moussa Ghaddar <78150453+moses0072@users.noreply.github.com>
2021-04-22 21:20:33 +0200

    fixed a bug in cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors({
 	origin: (origin, callback) => {
 		if (!origin) return callback(null, true);
 		if(allowedOrigins.indexOf(origin) === -1) {
-			let message = 'The CORS policy for this application doesn't allow from origin ' + origin;
+			let message = 'The CORS policy for this application doesn\'t allow from origin ' + origin;
 			return callback(new Error(message), false);
 		}
 		return callback(null, true);
