e7d2cd9513871728698deb009ebb89d50460c8bc
Moussa Ghaddar <78150453+moses0072@users.noreply.github.com>
2021-04-23 09:34:20 +0200

    fixed bug and declaird the app before cors

--- index.js
+++ index.js
@@ -11,16 +11,7 @@ const cors = require('cors');
 //List of allowed domains
 let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
 
-app.use(cors({
-	origin: (origin, callback) => {
-		if (!origin) return callback(null, true);
-		if(allowedOrigins.indexOf(origin) === -1) {
-			let message = 'The CORS policy for this application doesn\'t allow from origin ' + origin;
-			return callback(new Error(message), false);
-		}
-		return callback(null, true);
-	}
-}));
+
 
 const Movies = Models.Movie;
 const Users = Models.User;
@@ -36,6 +27,17 @@ const app = express();
 
 app.use(bodyParser.json());
 let auth = require('./auth')(app);
+
+app.use(cors({
+	origin: (origin, callback) => {
+		if (!origin) return callback(null, true);
+		if(allowedOrigins.indexOf(origin) === -1) {
+			let message = 'The CORS policy for this application doesn\'t allow from origin ' + origin;
+			return callback(new Error(message), false);
+		}
+		return callback(null, true);
+	}
+}));
 
 //mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
