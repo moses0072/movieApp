ad654d3f20d899e1361d3159b566a2e78455228e
Moussa Ghaddar <78150453+moses0072@users.noreply.github.com>
2021-05-04 17:17:12 +0200

    removed the authentication from the users

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ app.delete('/users/:Username', passport.authenticate('jwt', { session: false}), 
 });
 
 //Get all users
-app.get('/users', passport.authenticate('jwt', { session: false}), (req, res) => {
+app.get('/users', (req, res) => {
 	Users.find()
 	.then((users) => {
 		res.status(201).json(users);
