01f1895d05296e19eafedffff98303fc8c9b24d3
Moussa Ghaddar <78150453+moses0072@users.noreply.github.com>
2021-05-05 10:12:57 +0200

    changed mongoose connect

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ app.use(cors());
 	}
 }));*/
 
-//mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 //Return the documentation html
