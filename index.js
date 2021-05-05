e7e2ad3d1cf57b4e758ed517256107a3e0bf1e19
Moussa Ghaddar <78150453+moses0072@users.noreply.github.com>
2021-05-05 12:02:08 +0200

    fixed cors

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const app = express();
 
 //Cors access (allowed domains)
 const cors = require('cors');
-app.use(cors());
+//app.use(cors());
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+};
+app.use(cors(corsOptions));
 
 const {check, validationResult} = require('express-validator');
 
